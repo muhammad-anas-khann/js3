7b8788321f034e03219685cd0e80c0bc5d2ba8b9
Muhammed Anas Khan <manaskhan528@gmail.com>
2024-12-25 09:58:27 +0500

    .

---
+++ app.js
@@ -0,0 +1,241 @@
+
+// chapter 6
+// question 3
+
+// let name = prompt(`enter your name`)
+
+// alert(`Hello ${name}`)
+
+
+// question 6
+// let subject1 = +prompt(`enter subject 1`)
+// let subject2 = +prompt(`enter subject 2`)  
+// let subject3 = +prompt(`enter subject 3`)  
+
+// const totalMark = 100
+
+
+// document.write(`<h1>Subject   Total Marks     Obtained Marks      Percentage</h1>`)
+
+// document.write(`English:&nbsp; &nbsp; &nbsp;&nbsp; ${totalMark} &nbsp; &nbsp; &nbsp;&nbsp; ${subject1} &nbsp; &nbsp; &nbsp;&nbsp; ${subject1 * 100 / totalMark}%`)
+// document.write(`<br>Maths: &nbsp; &nbsp; &nbsp;&nbsp; ${totalMark} &nbsp; &nbsp; &nbsp;&nbsp; ${subject2}  &nbsp; &nbsp; &nbsp;&nbsp;  ${subject2 * 100 / totalMark}%`)
+// document.write(`<br>Science: &nbsp; &nbsp; &nbsp;&nbsp;${totalMark} &nbsp; &nbsp; &nbsp;&nbsp; ${subject3}  &nbsp; &nbsp; &nbsp;&nbsp;  ${subject3 *  100 / totalMark}%`)
+
+
+
+// chapter 9-11
+// question 1
+// let city = prompt(`enter city name`)
+// if(city == "karachi")
+
+//     alert(`WELCOME TO THE CITY OF LIGHTS`)
+
+
+
+
+// let gender = prompt(`enter your gender`)
+// if(gender == `male`)
+
+
+//     alert(`goodmorning sir`)
+
+//     if(gender == `female`)
+//         alert( `goodmorning ma'am`)
+
+
+
+// chapter 9
+// question 6
+
+
+// let totalMarks = +prompt(`enter total marks`)
+
+
+// if(totalMarks >= 80)
+//     alert(`you got A grade`)
+
+// if(totalMarks >= 60)
+//     alert(`you got B grade`)
+
+// if(totalMarks >= 40)
+//     alert(`you need to improve`)
+
+
+// if(totalMarks >= 0)
+//     alert(`Sorry`)
+
+
+
+
+
+
+
+
+
+
+
+
+
+// let num = +prompt(`enter your amount`)
+
+// let inr = (3.28)
+// let usd = (277)
+
+// let conversion = prompt(`where you want to convert your currency`)
+
+// if(conversion  == `inr`){
+//     alert(num / inr)
+// }
+
+// if(conversion == `usd`){
+//     alert(num / usd)
+// }
+
+
+
+// let pkr = +prompt(`enter your pkr`)
+// let change = prompt('change into')
+// let inr = (3.28)
+// let usd = (278)
+
+
+
+
+
+
+// if(change === "usd" ){
+//     document.write(pkr/usd)
+
+// }
+
+// else if(change === "inr"){
+//     document.write(pkr/inr)
+// }
+
+// else {
+//     document.write("invalid input")
+
+// }
+
+
+
+
+
+
+
+// chapter 12 - 13
+// question 2
+
+
+
+// let a = +prompt()
+// let b = +prompt()
+
+
+// if(a > b){
+//     document.write(`your first number is greater ${a}`)
+// }
+
+
+// else if(b > a){
+//     document.write(`your second number is greater ${b}`)
+// }
+// else{
+//     document.write(`They both are same numbers`)
+
+// }
+
+
+
+
+// question 3
+//  let num = +prompt("Enter a number");
+
+//  if(num > 0){
+//     document.write(`your number is positive`)
+
+//  }
+//  else if(num < 0){
+//     document.write(`your number is negative`)
+
+//  }
+
+//  else{
+//     document.write(`your number is zero`)   
+//  }
+
+
+// question 5
+
+// let password = prompt("Enter your password");
+// let confirmPassword = prompt("Confirm your password");
+
+// if(password === confirmPassword){
+//     document.write(`Correct! The password you entered matches the original password`)
+// }
+// else{
+//     document.write("Password is incorrect")
+// }
+
+
+// question 6
+// var greeting;
+// var hour = 13;
+// if (hour < 18) {
+// greeting = "Good day";
+// } 
+// else {
+// greeting = "Good evening";
+// }
+
+
+// question 7
+
+// let time = +prompt(`enter your current time`);
+// if (time >= 0000 && time < 1200){
+//     document.write(`Good Morning Sir`)
+
+// }
+// else if(time >= 1201 && time < 2359){
+//     document.write(`Good Night Sir`)
+// }
+
+
+
+
+
+
+
+
+
+
+// let num = +prompt(`enter any number`)
+
+// let freindsdue = +prompt(`enter amount of freinds due`)
+
+// let myNum = (30)
+
+// let total =  (num +  freindsdue + myNum)
+
+// document.write(total / 2 - freindsdue)
+
+
+
+// chap 12 Q2
+
+// let a = +prompt(`value1`)
+// let b = +prompt(`value2`)
+
+// if(a > b){
+//     document.write(`${a} is greater than ${b}`)
+// }
+
+// else if (b > a) {
+//     document.write(`${b} is greater than ${a}`) 
+// } 
+
+// else{
+//     document.write('dono ek jese hein')
+// }
+
+
